4e982e9862a7274ab0059b114e73d0c8b15958ce
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-10-11 21:57:42 +0200

    chore: work

---
+++ _config.ts
@@ -0,0 +1,26 @@
+import {
+  GpioMapping,
+  LedMatrix,
+  LedMatrixUtils,
+  MatrixOptions,
+  PixelMapperType,
+  RuntimeOptions,
+} from "rpi-led-matrix";
+
+export const matrixOptions: MatrixOptions = {
+  ...LedMatrix.defaultMatrixOptions(),
+  rows: 32,
+  cols: 64,
+  chainLength: 2,
+  hardwareMapping: GpioMapping.Regular,
+  parallel: 1,
+  // pixelMapperConfig: LedMatrixUtils.encodeMappers(
+  //   { type: PixelMapperType.Chainlink }
+  // ),
+  // pixelMapperConfig: LedMatrixUtils.encodeMappers({ type: PixelMapperType.U }),
+};
+
+export const runtimeOptions: RuntimeOptions = {
+  ...LedMatrix.defaultRuntimeOptions(),
+  gpioSlowdown: 4,
+};
--- index.ts
+++
@@ -1,6 +0,0 @@
-const LedMatrix = require("rpi-led-matrix");
-
-const matrix = new LedMatrix(
-  LedMatrix.defaultMatrixOptions(),
-  LedMatrix.defaultRuntimeOptions()
-);
